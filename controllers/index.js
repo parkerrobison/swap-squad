38f47c00e912c2ee282c4bef9b2904c7c7c1f6b6
probison <parker.robison@hotmail.com>
2020-08-11 18:29:32 -0600

    reviewing emailer code

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const emailRoutes = require("./email-routes");
 
 router.use('/api', apiRoutes);
+router.use("/email", emailRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
