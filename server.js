0b30fa99226704f3583042c3244430dd783c9602
probison <parker.robison@hotmail.com>
2020-08-12 19:43:39 -0600

    made secret a variable

--- server.js
+++ server.js
@@ -12,9 +12,8 @@ const session = require("express-session");
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-
 const sess = {
-  secret: "We need to change this later",
+  secret: process.env.SESSION_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
