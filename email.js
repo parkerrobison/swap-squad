f87767e7d4a28067a95b0198eb10f3cd35a1b7e2
delimr <whoisraf.delima@users.noreply.github.com>
2020-08-13 14:01:59 -1000

    added feature sendgrid email with example mail

--- email.js
+++ email.js
@@ -1,38 +1,12 @@
-
-const nodemailer = require("nodemailer");
-const { primaryKeyAttribute } = require("./models/User");
-
-
-  async function sendEmail() {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-        user: 'swapsquadteam@gmail.com',
-        pass: 'boycpfiymxjvrscr',
-      },
-      
-    });
-
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      to: 'parker.robison@hotmail.com',
-      subject: 'Hello',
-      body: 'This is short',
-    });
-
-
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-  }
-
-  sendEmail().catch((err) => {
-    console.log(err);
-  });
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'test@example.com',
+  from: 'test@example.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
