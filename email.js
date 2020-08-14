60c1167c23fcb4ddd8d89f6cbbb7dc4683a165b6
delimr <whoisraf.delima@users.noreply.github.com>
2020-08-13 14:08:28 -1000

    change message for txt

--- email.js
+++ email.js
@@ -5,8 +5,8 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
   to: 'test@example.com',
   from: 'test@example.com',
-  subject: 'Sending with Twilio SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  subject: 'Swap Squad - Somebody wants to trade!',
+  text: 'Looks like somebody wants to trade with you. please check your message',
+  html: '<strong>Swap Squad! Making trade fun and easy</strong>',
 };
 sgMail.send(msg);
