0320138370cf7f096badc4463af528e0f6973faa
Jacob Corum <cor15031@byui.edu>
2020-08-17 21:12:11 -0600

    added form verification

--- public/js/add-post.js
+++ public/js/add-post.js
@@ -5,9 +5,22 @@ async function newFormHandler(event) {
   const post_title = document.querySelector("#post-title").value.trim();
   const post_text = document.querySelector("#post-text").value.trim();
   const item_name = document.querySelector("#item-name").value.trim();
-  const item_description = document.querySelector("#item-description").value.trim();
+  const item_description = document
+    .querySelector("#item-description")
+    .value.trim();
   const item_quantity = document.querySelector("#item-quantity").value.trim();
   const item_condition = document.querySelector("#item-condition").value.trim();
+  if (
+    !post_title ||
+    !post_text ||
+    !item_name ||
+    !item_quantity ||
+    !item_condition ||
+    !item_description
+  ) {
+    alert("Please fill out all required fields");
+    return;
+  }
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
